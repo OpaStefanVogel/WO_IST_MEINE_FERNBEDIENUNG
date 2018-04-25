cdba8d3533ae58170a2420d80f63830012880793
Stefan Vogel <backebackekuchen16@gmail.com>
2018-04-25 23:40:44 +0200

    Test xslt über github

---
+++ REZEPT.xsl
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:template match="/REZEPT">
+<html>
+<link rel="stylesheet" type="text/css" href="REZEPT.css" />
+
+ <head></head>
+ <body class="REZEPT">
+<xsl:apply-templates />
+<script src="REZEPT_UHR.js" type="text/javascript"/>
+ </body></html>
+</xsl:template>
+
+<xsl:template match="UHR">
+<span class="UHR" ontouchstart="CLICKI(event)"
+  ontouchmove="DRAGGI(event)"
+  angehalten="ja"><xsl:attribute name="Anfangszeit"><xsl:value-of select="." /></xsl:attribute>
+  <img src="REZEPT_UHR.svg"/>
+  <xsl:value-of select="." />
+  </span>
+</xsl:template>
+
+<xsl:template match="TITEL">
+<div><span style="vertical-align:top">Rezept </span>
+<svg style="border:solid" width="100%" height="110px" font-family="Helvicta">
+<text transform="scale(6,4) rotate(0) translate(0,16)" stroke="red" fill="green">
+  * *    * <xsl:value-of select="." /> * * *
+   <xsl:value-of select="." /> * * *
+    <xsl:value-of select="." /> * * *
+     <xsl:value-of select="." /> * * *
+      <xsl:value-of select="." /> * * *
+    <animate attributeType="XML" attributeName="x"     
+    begin="0s" dur="200s"   
+    from="0" to="-800"
+    accumulate="sum"
+    additive="sum"
+    repeatCount="50"    
+    fill="freeze" />
+    
+ </text>
+</svg>
+</div>
+</xsl:template>
+
+<xsl:template match="ZUTATEN">
+ <span class="ZUTATEN"><xsl:value-of select="." /></span>
+</xsl:template>
+
+<xsl:template match="ZUBEREITUNG">
+  
+ <span class="ZUBEREITUNG"><xsl:apply-templates /></span>
+</xsl:template>
+
+<xsl:template match="ZUBEREITUNG/a">
+ <a><xsl:attribute name="href"><xsl:value-of select="." />.xml</xsl:attribute>
+  <u><xsl:value-of select="." /></u></a>
+ </xsl:template>
+
+<xsl:template match="BILD">
+ <img><xsl:attribute name="src"><xsl:value-of select="." /></xsl:attribute></img>
+ </xsl:template>
+
+<xsl:template match="a">
+ <a><xsl:attribute name="href"><xsl:value-of select="." />.xml</xsl:attribute>
+  <u><xsl:value-of select="." /></u></a>;
+ </xsl:template>
+
+</xsl:stylesheet>
---
+++ REZEPT_UHR.js
@@ -0,0 +1,86 @@
+  var SPANS=document.getElementsByTagName("span");
+  var UHREN=Array();
+//  alert(SPANS.length);
+  for (j=0;j<SPANS.length;j++) {
+    if (SPANS.item(j).getAttribute("class")=="UHR") {
+      UHREN.push(SPANS.item(j));
+      }
+    }
+//  alert(UHREN.length);
+var AKTUELLE_ZEIT=0;
+var VORHERIGE_ZEIT=0;
+var ZIELZEIT=0;
+var YPOS=0;
+function Kartoffeluhr(){
+  AKTUELLE_ZEIT=Date.now();
+  for (j=0;j<UHREN.length;j++) {
+    node=UHREN[j];
+    if (node.getAttribute("angehalten")=="nein") {
+      ZIELZEIT=node.getAttribute("Zielzeit");
+      DIFFERENZ=ZIELZEIT-AKTUELLE_ZEIT;
+      if (DIFFERENZ<0) {
+        DIFFERENZ=-DIFFERENZ;
+//        node.setAttribute("angehalten","ja");
+        node.setAttribute("style","background:blue");
+        }
+      NOCHZEIT=new Date(DIFFERENZ);
+      if (NOCHZEIT.getSeconds()<10) {TB=":0"} else { TB = ":" };
+      node.lastChild.nodeValue=NOCHZEIT.getMinutes()+TB+NOCHZEIT.getSeconds();
+      }
+    }
+  }
+setInterval(Kartoffeluhr,999);
+
+function CLICKI(event) {
+  event.preventDefault();
+  node=event.target;
+  YPOS=event.touches[0].screenY;
+  AKTUELLE_ZEIT=Date.now();
+  if (AKTUELLE_ZEIT<VORHERIGE_ZEIT+600) {
+    //alert("doppelclick");
+    node.setAttribute("angehalten","ja");
+    node.lastChild.nodeValue=node.getAttribute("Anfangszeit");    
+    node.setAttribute("style","");
+    } else {
+  NOCH_ZEIT=node.lastChild.nodeValue.split(":");
+//  alert(NOCH_ZEIT[0]*60000+NOCH_ZEIT[1]*1000);
+  ZIELZEIT=AKTUELLE_ZEIT+NOCH_ZEIT[0]*60000+NOCH_ZEIT[1]*1000;
+  node.setAttribute("Zielzeit",ZIELZEIT);
+//alert(node.getAttribute("Zielzeit"));
+  if (node.getAttribute("angehalten")=="ja") {
+    node.setAttribute("angehalten","nein");
+    node.setAttribute("style","background:yellow; outline:solid red");
+    } else {
+      node.setAttribute("angehalten","ja");
+      node.setAttribute("style","background:yellow;");
+      }
+      }
+  VORHERIGE_ZEIT=AKTUELLE_ZEIT;
+  }
+function DRAGGI(event) {
+  event.preventDefault();
+  node=event.target;
+  node.setAttribute("angehalten","ja");
+  if (YPOS <= event.touches[0].screenY) {//alert(node.getAttribute("Merkzeit"));
+    node.lastChild.nodeValue=node.getAttribute("Merkzeit");
+    } else {
+      node.setAttribute("Merkzeit",node.lastChild.nodeValue);
+      node.lastChild.nodeValue=node.getAttribute("Anfangszeit");
+      }
+  node.setAttribute("style","");
+  }
+
+
+
+
+
+//fuer TIME:
+function CLICKTIME() {
+  event.preventDefault();
+  node=event.target;
+//  alert(node.lastChild.nodeValue.split(":","T"));
+  CLICKTIMEOBJ=new Date(node.lastChild.nodeValue);
+  alert(CLICKTIMEOBJ.toString());
+  }
+
+
